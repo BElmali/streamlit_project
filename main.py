383c88ec113a751e509232aa47e97f815927af6b
Burak ELMALI <burakelmali1@gmail.com>
2024-04-07 21:14:45 +0300

    Proje

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 import numpy as np
-
+import requests
 
 st.set_page_config(layout = "wide", page_title="Steam Game Recommend", page_icon="")
 
@@ -17,7 +17,11 @@ def get_pipeline():
     pipeline = joblib.load('knn_game_recommender_pipeline.pkl')
     return pipeline
 
-
+@st.cache_data
+def fetch_image_as_bytes(steam_id: int):
+    url = f'https://cdn.akamai.steamstatic.com/steam/apps/{steam_id}/header.jpg'
+    response = requests.get(url)
+    return response.content
 
 
 st.title(":rainbow[Steam Game Recommend]")
@@ -34,30 +38,26 @@ empty_col1, empty_col2, empty_col3 = random_tab.columns([4,3,2])
 
 if empty_col2.button("Rastgele Oyun Öner"):
 
-    random_songs = df[~df["title"].isna()].sample(5)
+    random_game = df[~df["title"].isna()].sample(5)
 
     for i, col in enumerate(columns):
 
-        #col.image(random_songs.iloc[i]['url'])
-        col.write(f"**{random_songs.iloc[i]['title']}**")
+        col.image(fetch_image_as_bytes(random_game.iloc[i]['app_id']))
+        col.write(f"**{random_game.iloc[i]['title']}**")
 
 # Öneri Sistemi
 
 pipeline = get_pipeline()
-drop_columns=['2D', '3D', 'Action-Adventure', 'Anime',
-        'Building', 'Cartoony',
-       'Choices Matter', 'Co-op', 'Colorful', 'Combat', 'Comedy',
-       'Controller', 'Cute', 'Dark', 'Difficult', 'Early Access',
-       'Exploration', 'FPS', 'Family Friendly', 'Fantasy',
+drop_columns=['2D', '3D',  'Anime',  'Co-op', 'Colorful',  'Comedy',
+       'Cute', 'Difficult', 'Early Access',
+       'Exploration',  'Family Friendly', 'Fantasy',
        'Female Protagonist', 'First-Person', 'Free to Play', 'Funny',
-       'Gore', 'Great Soundtrack', 'Horror',  'Linear',
-       'Management', 'Minimalist',  'Multiple Endings',
-       'Mystery', 'Nudity', 'Open World', 'Physics', 'Pixel Graphics',
-       'Platformer', 'Point & Click', 'Psychological Horror',
-       'PvP',  'Realistic', 'Relaxing', 'Retro', 'Sandbox',
+       'Gore', 'Great Soundtrack', 'Horror',
+        'Open World',  'Pixel Graphics',
+       'Platformer',
+       'Relaxing', 'Retro',
        'Sci-fi', 'Shooter',
-        'Stylized', 'Survival', 'Tactical',
-       'Third Person', 'Top-Down', 'VR', 'Violent', 'Visual Novel',
+       'Third Person',  'Violent',
        'app_id', 'tags', 'title']
 recom_df=df.drop(columns=drop_columns, axis=1)
 col_features1, col_features2, col_recommendation = recommendation_tab.columns(3)
@@ -83,35 +83,24 @@ features = np.array([int(Indie), int(Singleplayer), int(Casual), int(Action),
                      int(Story_Rich)]).reshape(1, -1)
 
 if col_features2.button("Öneri Getir!"):
-
     distances, indices = pipeline.named_steps['knn'].kneighbors(pipeline.named_steps['scaler'].transform(features), n_neighbors=6)
-
     recommended_index = indices[0][1]
     recommended_song = df.iloc[recommended_index]
-
-    #col_recommendation.image(recommended_song['url'])
+    col_recommendation.image(fetch_image_as_bytes(recommended_song['app_id']))
     col_recommendation.write(f"**{recommended_song['title']}**")
 
-
+#Anasayfa
 col1, col2 = main_tab.columns(2)
 with col1:
     games_list = df['title'].tolist()
     selected_games = col1.multiselect("Oyun ismi girin veya seçin:", games_list)
-
     if col1.button("Benzer Oyunları Bul"):
         from sklearn.metrics.pairwise import cosine_similarity
-
         for selected_game in selected_games:
-            # Seçilen oyunun index'ini bul
             selected_game_index = df[df['title'] == selected_game].index[0]
-
-            # Oyun ismine göre benzerlik skorlarını hesapla
             similarity_scores = cosine_similarity(recom_df, recom_df.iloc[[selected_game_index]])
-
-            # Benzerlik skorlarını sırala ve en benzer olanları al
             similar_games_indices = np.argsort(similarity_scores.squeeze())[::-1][1:5]
-
             similar_games = df.iloc[similar_games_indices]
-
             for index, game in similar_games.iterrows():
+                col2.image(fetch_image_as_bytes(game['app_id']))
                 col2.write(f"**{game['title']}**")
